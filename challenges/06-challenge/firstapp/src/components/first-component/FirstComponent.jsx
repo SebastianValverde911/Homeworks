881e3ad2685323e621d408f0abdaa7fcfe059f71
Sebastian Valverde <juansebas_9711@hotmail.com>
2024-09-25 13:52:18 +0200

    challenge 6

---
+++ challenges/06-challenge/firstapp/src/components/first-component/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstComponent = () => {
+    return(
+        <>
+            <h1>Counter</h1>
+            <span>10</span>
+        </>
+    )
+}
+
+export default FirstComponent;
