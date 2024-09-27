9552f18a72d6ca7a7e480e8c3ed9f42c446434ac
Sebastian Valverde <juansebas_9711@hotmail.com>
2024-09-27 10:53:34 +0200

    challenge 08

---
+++ challenges/08-challenge/firstapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentApp from './components/ComponentApp';
+
+function App() {
+  return (
+    <div>
+      <ComponentApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ challenges/08-challenge/firstapp/src/components/ComponentApp.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+//const categories = ['First Category','Second Category'];
+const ComponentApp = () =>  {
+    const [text,setText] = useState('');
+    const [categories,setCategories] = useState(['First Category','Second Category']);
+    
+    const handleText = (e) => {
+        setText(e.target.value);
+    }
+    
+    const handleAddCategory = ()  => {
+        setCategories([...categories,text]);
+        setText('');
+    }
+
+    return(
+        <>
+        <h1>GiftExpert</h1>
+        <ol>
+            {
+                categories.map((category,key)=>{
+                    return <li key={key}>{category}</li>
+                })
+            }
+        </ol>
+        <input
+            value={text}
+            onChange={e => handleText(e)} 
+        />
+        <button
+            onClick={()=>handleAddCategory()}
+            >Agregar categoria</button>
+        </>
+    )
+}
+
+export default ComponentApp;
